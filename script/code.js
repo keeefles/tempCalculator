5efecd4e37eb2f8bb2e36f0724da228c8e568b2e
Aakeefah Jones <aakeefahj@gmail.com>
2023-11-13 21:00:59 +0200

    lastUpdate

--- script/code.js
+++ script/code.js
@@ -6,7 +6,7 @@ celsius.oninput = () => {
     fahrenheit.value = parseFloat(output.toFixed(2));
 };
 fahrenheit.oninput = () => {
-    let output = (parseFloat(fahrenheit.value) * 9) / 5 + 32;
+    let output = (parseFloat(fahrenheit.value) - 32) * 5 / 9;
     celsius.value = parseFloat(output.toFixed(2));
 };
 
